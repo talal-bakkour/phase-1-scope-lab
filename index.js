2f03dd5610f147f633c90ca298b4efd9ca9f1931
talal <ta440237@gmail.com>
2023-05-07 18:34:47 +0300

    scope abbodi

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+ 
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+ 
+  leastFavoriteCustomer = 'someone else';
+
+  olikjopkplss
+}
+
